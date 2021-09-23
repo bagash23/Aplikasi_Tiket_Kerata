19295ddb496020d134e35b4dba2f6873fb6e26b4
Parakacu <parakacu@gmail.com>
2021-09-24 06:27:58 +0700

    Tiket Register

---
+++ tiket.py
@@ -0,0 +1,28 @@
+def start():
+    nama = input("Masukan nama anda: ")
+    hp = input("Masukan nomor HP anda: ")
+    ktp = int(input("Masukan NIK anda: "))
+    print("===== Pertanyaan Mengenai Vaksinasi =====")
+    vaksin()
+
+def vaksin():
+    print("1. Apakah anda sudah vaksin?")
+    vaksin = input("Masukan pilihan anda (y/n): ")
+    if vaksin == "y":
+        lokasi()
+    elif vaksin == "n":
+        print("Maaf anda tidak dapat melanjutkan")
+
+def lokasi():
+    print("2. Dimana lokasi anda vaksin?")
+    lokasi = input("Masukan lokasi anda vaksin: ")
+    dosis()
+
+def dosis():
+    print("3. Sudah berapa dosis yang anda terima?")
+    dosis = input("Masukan jumlah vaksin yang sudah anda terima: ")
+    print("Kalau begitu, Selamat anda telah menerima " + dosis + "x vaksin")
+
+# output
+print("========== Selamat Datang Di PT KAI Indonesia ==========")
+start()
